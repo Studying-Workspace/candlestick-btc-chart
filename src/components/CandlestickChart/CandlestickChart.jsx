9934af518a7c1b96c9efa7a9946eaf1799d544ce
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2023-12-02 13:49:35 +0200

    fix data processing and representation

--- src/components/CandlestickChart/CandlestickChart.jsx
+++ src/components/CandlestickChart/CandlestickChart.jsx
@@ -1,17 +1,27 @@
 import React, {useEffect, useState} from "react";
 import ReactApexChart from "react-apexcharts";
-import {updateCandlestick, accumulateFromBatch} from "../../../utilities.js";
+import {accumulateFromBatch} from "../../../utilities.js";
 import styles from './CandlestickChart.module.css';
 
 export const CandlestickChart = ({data, initialData, isSeriesLoading, isInitialLoading}) => {
 
     let processData = (rawData) => {
         const processedData = [];
-        const DATA_PER_MINUTE = 5;
-        for(let i = 0; i < rawData.length; i += DATA_PER_MINUTE) {
+        const DATA_PER_MINUTE = 1;
+        for (let i = 0; i < rawData.length; i += DATA_PER_MINUTE) {
             processedData.push(accumulateFromBatch(rawData, i, DATA_PER_MINUTE));
         }
-        return processedData;
+        const len = processedData.length;
+        return processedData.reverse().slice(len - 60);
+    }
+
+    const getStateFormat = (givenData) => {
+        return processData(givenData).map((item) => {
+            return {
+                x: new Date(item[0]),
+                y: [...item.slice(1)]
+            }
+        })
     }
 
     const [series, setSeries] = useState([
@@ -19,48 +29,39 @@ export const CandlestickChart = ({data, initialData, isSeriesLoading, isInitialL
             name: "series-1",
             // array of objects with x and y properties
             // {x: new Date(1991, 0, 1), y: [30, 40, 45, 50]},
-            data: processData(initialData).map((item) => {
-                return {
-                    x: new Date(item[0]),
-                    y: [...item.slice(1)]
-                }
-            })
+            data: getStateFormat(initialData)
         }
     ]);
 
     // chart: open - high - low - close
     // api: timestamp - low - high - open - close - _volume
     useEffect(() => {
-        if(isSeriesLoading) {
+        if (isSeriesLoading) {
             return;
         }
-        let newSeries = [...series];
-        const initialCandlestickData = accumulateFromBatch(data, 0, 50);
-        newSeries[0].data.push({x: new Date(initialCandlestickData[0]), y: [...initialCandlestickData.slice(1)]});
-        if (newSeries[0]?.data.length > 60) {
-            newSeries[0].data.unshift();
-        }
-        setSeries(newSeries);
-        updateCandlestick([...series], setSeries, [...data]);
+        setSeries([{
+            name: "series-1",
+            data: getStateFormat(data)
+        }]);
     }, [data]);
 
 
     return (
         <div className={styles.chartBox}>
             <div className="row">
-                    <ReactApexChart
-                        options={{
-                            chart: {
-                                id: "basic-bar"
-                            },
-                            xaxis: {
-                                categories: [] // empty array
-                            }
-                        }}
-                        series={series}
-                        type="candlestick" // change type to candlestick
-                        width="800"
-                    />
+                <ReactApexChart
+                    options={{
+                        chart: {
+                            id: "basic-bar"
+                        },
+                        xaxis: {
+                            categories: [] // empty array
+                        }
+                    }}
+                    series={series}
+                    type="candlestick" // change type to candlestick
+                    width="800"
+                />
             </div>
         </div>
     );
