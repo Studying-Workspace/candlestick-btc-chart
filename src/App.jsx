ae2aa9041cf7f7fad1cd3991284e2ba21a095260
Noran Sherif <>
2023-12-02 15:05:44 +0200

    add countdown component

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,50 @@
-import './App.css'
-import {CandlestickChart} from "./components/CandlestickChart/CandlestickChart.jsx";
-import {useQuery} from "@tanstack/react-query";
+import "./App.css";
+import { CandlestickChart } from "./components/CandlestickChart/CandlestickChart.jsx";
+import { useQuery } from "@tanstack/react-query";
 import Spinner from "./components/Spinner/Spinner.jsx";
+import { CountDown } from "./components/CountDown/CountDown.jsx";
 
 function App() {
+  const { isLoading: isSeriesLoading, data: seriesData } = useQuery({
+    queryKey: ["btc-price-1m"],
+    queryFn: async () => {
+      const res = await fetch(
+        "https://api.pro.coinbase.com/products/BTC-USD/candles/1m"
+      );
+      return await res.json();
+    },
+    refetchInterval: 60 * 1000,
+    refetchIntervalInBackground: true,
+    refetchOnWindowFocus: true,
+  });
 
-    const {isLoading: isSeriesLoading, data: seriesData} = useQuery({
-        queryKey: ['btc-price-1m'],
-        queryFn: async () => {
-            const res = await fetch('https://api.pro.coinbase.com/products/BTC-USD/candles/1m');
-            return await res.json();
-        },
-        refetchInterval: 60 * 1000,
-        refetchIntervalInBackground: true,
-        refetchOnWindowFocus: true,
-    });
+  const { isLoading: isInitialLoading, data: initialData } = useQuery({
+    queryKey: ["btc-initial-price"],
+    queryFn: async () => {
+      const res = await fetch(
+        "https://api.pro.coinbase.com/products/BTC-USD/candles/1m"
+      );
+      return await res.json();
+    },
+  });
 
-    const {isLoading: isInitialLoading, data: initialData} = useQuery({
-        queryKey: ['btc-initial-price'],
-        queryFn: async () => {
-            const res = await fetch('https://api.pro.coinbase.com/products/BTC-USD/candles/1m');
-            return await res.json();
-        }
-    });
-
-    return (
-        <>
-            {isSeriesLoading || isInitialLoading
-                ? <Spinner/>
-                : <CandlestickChart
-                    data={seriesData}
-                    initialData={initialData}
-                    isSeriesLoading={isSeriesLoading}
-                    isInitialLoading={isInitialLoading}/>}
-        </>
-    )
+  return (
+    <>
+      {isSeriesLoading || isInitialLoading ? (
+        <Spinner />
+      ) : (
+        <div>
+          <CountDown seconds={60} />
+          <CandlestickChart
+            data={seriesData}
+            initialData={initialData}
+            isSeriesLoading={isSeriesLoading}
+            isInitialLoading={isInitialLoading}
+          />
+        </div>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/CountDown/CountDown.jsx
+++ src/components/CountDown/CountDown.jsx
@@ -1,5 +1,18 @@
-export const CountDown = () => {
-    return (
-        <></>
-    )
-}
+import { useRef, useState, useEffect } from "react";
+export const CountDown = ({ seconds }) => {
+  const [countDown, setCountDown] = useState(seconds);
+  const timerId = useRef();
+
+  useEffect(() => {
+    timerId.current = setInterval(() => {
+      setCountDown((prev) => (prev === 1 ? seconds : prev - 1));
+    }, 1000);
+    return () => clearInterval(timerId.current);
+  }, []);
+  return (
+    <div>
+      <span>Until next update: </span>
+      <span>{countDown === 60 ? "1m 00s" : `0m ${countDown}s`}</span>
+    </div>
+  );
+};
