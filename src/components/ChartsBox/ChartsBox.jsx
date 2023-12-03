2972475a4cd475e8840334fb78fd8734a9b0bb0d
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2023-12-03 12:10:48 +0200

    remove unnecessary imports from charts box

--- src/components/ChartsBox/ChartsBox.jsx
+++ src/components/ChartsBox/ChartsBox.jsx
@@ -1,7 +1,6 @@
 import {CountDown} from "../CountDown/CountDown";
 import {CandlestickChart} from "../CandlestickChart/CandlestickChart";
 import styles from "./ChartsBox.module.css";
-import SelectCoin from "../SelectCoin/SelectCoin";
 import DarkLightModeButton from "../DarkLightModeButton/DarkLightModeButton";
 import {ChartOptions} from "../ChartOptions/ChartOptions.jsx";
 
