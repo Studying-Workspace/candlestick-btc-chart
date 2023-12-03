203f117eb0bb4583bab40c06cc74bd5694156a8f
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2023-12-03 12:19:36 +0200

    refactor select menu to be more generic

--- src/components/ChartOptions/ChartOptions.jsx
+++ src/components/ChartOptions/ChartOptions.jsx
@@ -1,9 +1,15 @@
-import SelectCoin from "../SelectCoin/SelectCoin.jsx";
+import SelectMenu from "../SelectMenu/SelectMenu.jsx";
+import {coins} from '../../supported-coins';
 
 export const ChartOptions = ({coin, setCoin, isDark}) => {
     return (
         <>
-            <SelectCoin coin={coin} setCoin={setCoin} isDark={isDark}/>
+            <SelectMenu defaultValue={coin}
+                        setCoin={setCoin}
+                        isDark={isDark}
+                        helperText='Choose a coin to show its price changes'
+                        menuItems={coins}
+            />
         </>
     )
 }
